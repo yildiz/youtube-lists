d1191cc1522a491369a8c57360df9531a7f13bdf
Volkan Yıldız <siryildiz@gmail.com>
2020-08-10 03:17:30 +0300

    logout ile bir şeyler yaptım, SSR ile logout işlevi ekledim ama ne gerek vardı?

--- src/pages/api/auth/logout.js
+++ src/pages/api/auth/logout.js
@@ -1,7 +1,11 @@
 export default (req, res) => {
-  res.setHeader(
-    'Set-Cookie',
-    'session=; Max-Age=0; SameSite=Strict; HttpOnly; Path=/'
-  );
-  res.status(200).end();
+	console.log('api/auth/logout');
+	console.log(req.headers.cookie);
+	res.setHeader(
+		'Set-Cookie',
+		'session=; Max-Age=0; SameSite=Strict; HttpOnly; Path=/',
+	);
+	// res.writeHead(302, { Location: '/' });
+	// res.end();
+	res.status(200).end();
 };
--- src/pages/logout.js
+++ src/pages/logout.js
@@ -2,14 +2,19 @@ import { useEffect } from 'react';
 import { useAuth } from '@providers/Auth';
 import withAuth from '@hocs/withAuth';
 
+import Cookies from 'js-cookie';
+
 import api from '@api/api';
 
-export default withAuth(function Logout() {
+const isServer = () => typeof window === 'undefined';
+
+function Logout() {
 	const { setAuthenticated } = useAuth();
 	useEffect(() => {
 		async function doLogout() {
 			await api.get('auth/logout').then((response) => {
 				if (response.status === 200) {
+					Cookies.set('v', 'y');
 					setAuthenticated(false);
 				} else {
 					console.error('Failed to logout', response);
@@ -19,4 +24,31 @@ export default withAuth(function Logout() {
 		doLogout();
 	}, [setAuthenticated]);
 	return <h1>Logging out</h1>;
-}, '/');
+}
+
+Logout.getInitialProps = async (ctx) => {
+	await new Promise((resolve) => {
+		setTimeout(resolve, 2500);
+	});
+
+	if (isServer()) {
+		ctx.res.setHeader(
+			'Set-Cookie',
+			'session=; Max-Age=0; SameSite=Strict; HttpOnly; Path=/',
+		);
+		ctx.res.writeHead(302, { Location: '/' });
+		ctx.res.end();
+	}
+
+	// // console.log(ctx.req);
+	// // console.log('ctx.req', new Date());
+	// // return;
+	// console.log(ctx.req.headers.cookie);
+
+	// await api.get('auth/logout', {
+	// 	headers: {
+	// 		Cookie: ctx.req.headers.cookie,
+	// 	},
+	// });
+};
+export default withAuth(Logout, '/');
